daa7c769c1fef950da26e24b85b3882bcf9e2083
DizzyRogue00 <ZHOU0329@e.ntu.edu.sg>
2026-03-20 17:10:39 +0800

    camera_model sparse_graph complete

---
+++ src/camera_model/src/sparse_graph/Transform.cpp
@@ -0,0 +1,68 @@
+#include <cameraModelCal/sparse_graph/Transform.h>
+
+namespace cameraModelCal
+{ 
+
+Transform::Transform()
+{
+    m_q.setIdentity();
+    m_t.setZero();
+}
+
+
+Transform::Transform(const Eigen::Matrix4d& H)
+{
+    m_q = Eigen::Quaterniond(H.block<3,3>(0,0));
+    m_t = H.block<3,1>(0,3);
+}
+
+Eigen::Quaterniond& Transform::rotation(void)
+{
+    return m_q;
+}
+
+const Eigen::Quaterniond& Transform::rotation(void) const
+{
+    return m_q;
+}
+
+double* Transform::rotationData(void)
+{
+    return m_q.coeffs().data();
+}
+const double* Transform::rotationData(void) const
+{
+    return m_q.coeffs().data();
+}
+
+Eigen::Vector3d& Transform::translation(void)
+{
+    return m_t;
+}
+
+const Eigen::Vector3d& Transform::translation(void) const
+{
+    return m_t;
+}
+
+double* Transform::translationData(void)
+{
+    return m_t.data();
+}
+
+const double* Transform::translationData(void) const
+{
+    return m_t.data();
+}
+
+Eigen::Matrix4d Transform::tomatrix(void) const
+{
+    Eigen::Matrix4d H;
+    H.setIdentity();
+    H.block<3,3>(0,0) = m_q.toRotationMatrix();
+    H.block<3,1>(0,3) = m_t;
+    return H;
+}
+
+
+}
